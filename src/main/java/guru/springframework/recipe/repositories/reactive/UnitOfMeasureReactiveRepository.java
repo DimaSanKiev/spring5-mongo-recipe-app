55ab411cba7e523b9e4bf5e47dbececd9e6bd8ac
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2017-10-30 21:49:13 +0100

    Add reactive dependencies and reactive repository

---
+++ src/main/java/guru/springframework/recipe/repositories/reactive/UnitOfMeasureReactiveRepository.java
@@ -0,0 +1,7 @@
+package guru.springframework.recipe.repositories.reactive;
+
+import guru.springframework.recipe.model.UnitOfMeasure;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+public interface UnitOfMeasureReactiveRepository extends ReactiveMongoRepository<UnitOfMeasure, String> {
+}
